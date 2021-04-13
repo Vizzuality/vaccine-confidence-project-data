321b2d5e0914d50be7e281549e6cb1b748848d46
Greta <greta.carrete@vizzuality.com>
2021-04-13 17:37:48 +0200

    using imgur api to get the url and update map data with url to imgur

--- datawrapper_api.R
+++ datawrapper_api.R
@@ -1,6 +1,7 @@
 #devtools::install_github("munichrocker/DatawRappr")
 library(DatawRappr)
 library(magick)
+library(tidyr)
 # connect to the API and authenticate
 token = read.table(".env", sep = "=") %>% 
   filter(V1 == "DATAWRAPPER_TOKEN") %>% 
@@ -36,7 +37,7 @@ chart_id = "mgy8T" # this chart was created via the web and the visualise parame
 
 good_graph_visualize = dw_retrieve_chart_metadata(chart_id)
 #v_settings = good_graph_visualize$content$metadata$visualize
-
+#rename series to Strongly Agree, Strongly Disagree and Neither.
 for (i in 1:length(countries)){ #
   sel_country = countries[i]
   
@@ -71,11 +72,48 @@ for (i in 1:length(countries)){ #
   dw_publish_chart(chart_id = chart_id)
   #export
   p = dw_export_chart(chart_id, type = "png", unit = "px", width = 400, height = 240, scale = 1, plain = TRUE)
-  image_write(p, path = paste0(sel_country,"importance.png"), format = "png")
+  image_write(p, path = paste0("charts_tooltip/",sel_country,"_importance.png"), format = "png")
 }
 
+# upload images to imgur, then get the name and the link from the uploaded images.
+library(httr)
+
+token_imgur = read.table(".env", sep = "=") %>% 
+  filter(V1 == "IMGUR_TOKEN") %>% 
+  select(V2)
+bearer = paste("Bearer",token_imgur, sep = " ")
+# there are several pages with data
+getCountInJson <- httr::GET("https://api.imgur.com/3/account/gretacv/images/count",
+                           accept_json(), 
+                           add_headers('Authorization' = bearer))
+
+res = list()
+for (i in 0:as.integer(content(getCountInJson)$data/50)){
+url = paste0('https://api.imgur.com/3/account/gretacv/images/', i)
+getInfoInJson <- httr::GET(url, 
+                           accept_json(), 
+                           add_headers('Authorization' = bearer))
 
 
+res[[i+1]] = ldply(lapply(content(getInfoInJson, as = "parsed")$data, unlist))
+
+
+}
+res_all = ldply(res) %>% 
+  select(id, name) %>% 
+  separate(name, 
+           sep = "_", 
+           into = c("country.or.territory", "question")) %>% 
+  rename("imgur_id" = "id")
+
+dim(res_all)
+head(res_all)
+# add the link information to the mean strongly agree df that is used for the map
+d_map = d %>% 
+  filter(response == "strongly agree" & time == 2020) %>% 
+  select(country.or.territory,  mean) %>% 
+  left_join(res_all)
+write.csv(d_map, "world_important_stronglyagree_2020.csv", row.names = FALSE)
 
 ############
 ## testing
